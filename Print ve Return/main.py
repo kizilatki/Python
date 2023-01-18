1844c179ea76de9b55987a5d6be9cfd4bfc4463a
Kızıl Atkı <kizilatki@hotmail.com>
2023-01-19 00:47:13 +0300

    Print&Return commands
    Differences between print and return commands with examples

---
+++ Print ve Return/main.py
@@ -0,0 +1,42 @@
+"""
+A differences between print and return function
+
+Using the print function, in general, provide to write (print) the result or anything that you want.
+
+However, return command to provide to end the function and so It serves to return a value to the place where the function was called.
+
+
+
+Easy level example:
+
+print(2+3)
+>> 5
+
+def addition(x+y):
+    return x+y
+
+addition(5,10):
+>> 15
+
+Medium level example:
+
+x = "Python"
+y = "Programming"
+z = "Language"
+t = x + y + z
+print(t)
+print("----------)
+
+>> PythonProgrammingLanguage
+>> ----------
+
+def big_number(x,y):
+    if x>y:
+        return x
+    elif y>x:
+        return y
+
+big_number(11,13)
+>> 13
+
+"""
