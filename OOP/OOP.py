61f83032f496cdca69972c2bd94c0be0f0262a34
Kızıl Atkı <77049951+kizilatki@users.noreply.github.com>
2023-01-28 21:56:51 +0300

    Object Oriented Programming

---
+++ OOP/OOP.py
@@ -0,0 +1,44 @@
+class Flying():
+    company = "THY"
+
+    def __init__(self, code, start, arrival, time, capacity, passenger):
+        self.code = code
+        self.start = start
+        self.arrival = arrival
+        self.time = time
+        self.capacity = capacity
+        self.passenger = passenger
+
+    def announce(self):
+        return "{} code {}-{} flight will take {} times".format(
+            self.code,
+            self.start,
+            self.arrival,
+            self.time)
+
+    def update_capacity(self):
+        return self.capacity - self.passenger
+
+    def ticket_sale(self, ticket_amount=1):
+        if self.passenger + ticket_amount <= self.capacity:  # Bütün özellikleri if kümesine atadım.
+            self.passenger += ticket_amount
+            self.update_capacity()
+
+            print("{} amount ticket is sold, rest of the amount: {}".format(
+                ticket_amount,
+                self.update_capacity()))
+
+        else:
+            print("Payment failed! Insufficient seats amount")
+
+    def ticket_cancel(self, ticket_amount=1):
+        if self.passenger >= ticket_amount:
+            self.passenger -= ticket_amount
+            self.update_capacity()
+
+            print("{} amount ticket cancelled, current seats number: {}".format(
+                ticket_amount,
+                self.update_capacity()))
+
+        else:
+            print("Payment failed! Please enter a right ticket amount.")
